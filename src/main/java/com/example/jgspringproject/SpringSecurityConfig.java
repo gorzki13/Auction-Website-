d51e75d128ed977a155688f7f414af625457138a
gorzki13 <gorzkiewicz84@gmail.com>
2021-12-07 15:09:21 +0100

    lab09+

--- src/main/java/com/example/jgspringproject/SpringSecurityConfig.java
+++ src/main/java/com/example/jgspringproject/SpringSecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.jgspringproject;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
